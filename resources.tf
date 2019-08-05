c35941663762cc9b12f4d895ff673e7647a31d2c
Ricardo Pereira Costa <NB25116@novabase.pt>
2019-08-05 08:51:58 +0100

    created subnet1 and subnet2

--- resources.tf
+++ resources.tf
@@ -6,3 +6,15 @@ resource "aws_vpc" "environment-example-two"{
     Name = "terraform-aws-vpc-example-two"
   }
 }
+
+resource "aws_subnet" "subnet1"{
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block,3,1)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "eu-central-1a"
+}
+
+resource "aws_subnet" "subnet2"{
+  cidr_block = "${cidrsubnet(aws_vpc.environment-example-two.cidr_block,2,2)}"
+  vpc_id = "${aws_vpc.environment-example-two.id}"
+  availability_zone = "eu-central-1b"
+}
